52645d0f0db574874617c3485946db9a7c72e9f2
yywjk-dmn <2986567641@qq.com>
2023-12-19 20:54:52 +0800

    [新增]动态数组的头文件（.h）和源文件（.c）

---
+++ dynamicArray/dynamicArray.h
@@ -0,0 +1,7 @@
+#ifndef __DYNAMIC_ARRAY_H_
+#define __DYNAMIC_ARRAY_H_
+
+/* 避免头文件重复包含 */
+
+
+#endif //__DYNAMIC_ARRAY_H_
---
+++ dynamicArray/mian.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dynamicArray.h"
+
+
+
+int main()
+{
+    /* 静态数组 
+         1.不够灵活
+         2.可能会浪费空间
+    */
+
+
+
+    return 0;
+}
