6d118d0d26180f4c708ef729ba8ef2d76b3e44f2
yywjk-dmn <2986567641@qq.com>
2023-12-20 14:06:19 +0800

    [修改]引入尾指针 将效率提升到O(1)

--- LinkList/LinkList.c
+++ LinkList/LinkList.c
@@ -1,31 +1,135 @@
 #include <stdio.h>
 #include "LinkList.h"
+#include <stdlib.h>
+#include <string.h>
 
-
-
+/* 状态码 */
+enum STATUS_CODE
+{
+    ON_SUCCESS,
+    NULL_PTR,
+    MALLOC_ERROR,
+    INVALID_ACCESS,
+};
 
 /* 链表初始化 */
-int LinkListInit(LinkList **pList)
+int LinkListInit(LinkList ** pList)
 {
+    int ret = 0;
+    LinkList *list = (LinkList *)malloc(sizeof(LinkList) * 1);//分配空间
+    if(list == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    memset(list, 0, sizeof(LinkList) * 1);
+
+    list->head = (LinkNode *)malloc(sizeof(LinkNode) * 1);/* 头插入需要分配空间（有指针但不一定有值) */
+    if(list->head == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+
+    /* 清空脏数据 */
+    memset(list->head, 0,sizeof(LinkNode) * 1);
+
+    /* 好像有问题 */
+    list->head->data = 0;
+    list->head->next = NULL;
+    
+    /* 初始化的时候，尾指针 = 头指针 */
+    list->tail = list->head;
+
+    /* 链表的长度为0 */
+    list->len = 0;
+
+    /* 二级指针*/
+    *pList = list;
 
 }
 
 /* 链表头插 */
-int LinkListHeadInsert(LinkList *pList, ELEMENTTYPE val)
+int LinkListHeadInsert(LinkList * pList, ELEMENTTYPE val)
 {
-
+    return LinkListAppointPosInsert(pList, 0, val);
 }
 
 /* 链表尾插 */
-int LinkListTailInsert(LinkList *pList, ELEMENTTYPE val)
+int LinkListTailInsert(LinkList * pList, ELEMENTTYPE val)
 {
-
+    return LinkListAppointPosInsert(pList, pList->len, val);
 }
 
-/* 链表指定位置插入 */
-int LinkListAppointPosInsert(LinkList *pList,int pos, ELEMENTTYPE val)
+/* 链表指定位置插 */
+int LinkListAppointPosInsert(LinkList * pList, int pos, ELEMENTTYPE val)
 {
+    int ret;
+    if(pList == NULL)
+    {
+        return NULL_PTR;
+    }
 
+    if(pos < 0 || pos > pList->len)
+    {
+        return INVALID_ACCESS;
+    }
+
+    /* 封装一个结点 */
+    LinkNode * newNode = (LinkList *)malloc(sizeof(LinkNode) * 1);
+    if(newNode == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+
+    /* 清楚脏数据 */
+    memset(newNode, 0, sizeof(LinkNode) * 1);
+#if 0
+    newNode ->data = 0;
+    newNode-> 
+#endif
+    /* 赋值 */
+    newNode->data = val;
+
+#if 1
+    /* 头结点和虚拟头结点不一样 */
+    /* 从虚拟头结点开始遍历 */
+    LinkNode * travelNode = pList->head; /* pList是连接结点的线 */
+#else
+    LinkNode * travelNode = pList->head->next;
+#endif
+    /* 这种情况下需要更改尾指针 */
+    int flag = 0;
+    if(pos == pList->len)
+    {
+        travelNode = pList->tail;
+
+    }
+    else
+    {
+        while (pos)
+        {
+        travelNode = travelNode->next;/* 找到想要插入数据的位置 */
+        pos--;
+        }
+    }
+    newNode->next = travelNode->next;
+    travelNode->next = newNode;
+
+    if(flag)
+    {
+        pList->tail = newNode;
+    }
+
+    
+#if 0
+    /* 修改结点指向 */
+    /* 先断再连，先后再前 */
+    newNode->next = travelNode->next; 
+    travelNode->next = newNode;
+#endif
+    /* 更新链表长度 */
+    (pList->len)++;
+
+    return ret;
 }
 
 /* 链表头删 */
@@ -34,7 +138,7 @@ int LinkListHeadDel(LinkList * pList)
 
 }
 
-/* 链表尾删 */
+/* 链表尾删*/
 int LinkListTailDel(LinkList * pList)
 {
 
@@ -47,25 +151,55 @@ int LinkListDelAppointPos(LinkList * pList, int pos)
 }
 
 /* 链表删除指定数据 */
-int LinkListDelAppointData(LinkList * pList, ELEMENTTYPE *val)
+int LinkListDelAppointData(LinkList * pList, ELEMENTTYPE val)
 {
 
 }
 
 /* 获取链表的长度 */
-int LinkListGetLength(LinkList * pList, int *psize)
+int LinkListGetLength(LinkList * pList, int *pSize)
 {
+    int ret = 0;
+    if(pList == NULL)
+    {
+        return NULL_PTR;
+    }
+    if(pSize)
+    {
+        *pSize = pList->len;
+    }
 
+    return ret;
 }
 
 /* 链表的销毁 */
-int LinkListDestroy(LinkList * pList)
+int LinkListDestory(LinkList * pList)
 {
 
 }
-
-/* 链表遍历接口 */
+/* 链表的遍历 */
 int LinkListForeach(LinkList * pList)
 {
-    
+    int ret = 0;
+    if(pList == NULL)
+    {
+        return NULL_PTR;
+    }
+#if 0
+    /* travelNode 指向虚拟头结点 */ 
+    LinkNode * travelNode = pList->head;
+     while (travelNode != NULL)
+    {
+        travelNode = travelNode->next;
+        printf("travelNode->data:\n", travelNode->data);
+    }
+#else
+    LinkNode * travelNode = pList->head->next;
+    while (travelNode != NULL)
+    {
+        printf("travelNode->data:\n", travelNode->data);
+        travelNode = travelNode->next;
+    }
+#endif
+    return ret;
 }
--- LinkList/LinkList.h
+++ LinkList/LinkList.h
@@ -1,56 +1,61 @@
-#ifndef __LINKLIST_H_
-#define __LINKLIST_H_
+#ifndef __LinkList_H_
+#define __LinkList_H_
 
 #define ELEMENTTYPE int
-/* 链表结点取别名 */
-typedef struct LinkList
+/* 链表结点 取别名*/
+typedef struct LinkNode
 {
     ELEMENTTYPE data;
+
     /* 指向下一个结点的指针 */
-    struct LinkList *next;   /* 指针的类型为什么是结点 */
-} LinkNode;
+    struct LinkNode *next; /* 指针类型 */
 
-typedef struct  LINKLIST
+
+}LinkNode;
+
+/* 链表 */
+typedef struct LinkList
 {
-    /* 链表的虚拟头结点 */
-    LinkNode * head;
+    LinkNode * head;  /* 链表的虚拟头结点 */
+    LinkNode * tail;  /* 为什么尾指针不需要分配空间 */
+    int len;         /* 链表长度 */
 
-    /* 链表的长度 */
-    int len;
-    
-} LINKLIST;
+}LinkList;
+
+
 
 /* 链表初始化 */
-int LinkListInit(LinkList **pList);
+int LinkListInit(LinkList ** pList);
 
 /* 链表头插 */
-int LinkListHeadInsert(LinkList *pList, ELEMENTTYPE val);
+int LinkListHeadInsert(LinkList * pList, ELEMENTTYPE val);
 
 /* 链表尾插 */
-int LinkListTailInsert(LinkList *pList, ELEMENTTYPE val);
+int LinkListTailInsert(LinkList * pList, ELEMENTTYPE val);
 
-/* 链表指定位置插入 */
-int LinkListAppointPosInsert(LinkList *pList,int pos, ELEMENTTYPE val);
+/* 链表指定位置插 */
+int LinkListAppointPosInsert(LinkList * pList, int pos, ELEMENTTYPE val);
 
 /* 链表头删 */
 int LinkListHeadDel(LinkList * pList);
 
-/* 链表尾删 */
+/* 链表尾删*/
 int LinkListTailDel(LinkList * pList);
 
 /* 链表指定位置删除 */
 int LinkListDelAppointPos(LinkList * pList, int pos);
 
 /* 链表删除指定数据 */
-int LinkListDelAppointData(LinkList * pList, ELEMENTTYPE *val);
+int LinkListDelAppointData(LinkList * pList, ELEMENTTYPE val);
 
 /* 获取链表的长度 */
-int LinkListGetLength(LinkList * pList, int *psize);
+int LinkListGetLength(LinkList * pList, int *pSize);
 
 /* 链表的销毁 */
-int LinkListDestroy(LinkList * pList);
+int LinkListDestory(LinkList * pList);
 
 /* 链表遍历接口 */
 int LinkListForeach(LinkList * pList);
 
-#endif
+
+#endif //__LinkList_H_
