ceab13bfab0c959cded08ab71b9150f68c14b2fb
yywjk-dmn <2986567641@qq.com>
2023-12-25 16:16:45 +0800

    [修改]完善二叉搜索树的插入API接口

--- BinarySearchTree/binarySearchTree.c
+++ BinarySearchTree/binarySearchTree.c
@@ -38,6 +38,7 @@ int binarySearchTreeInit(BinarySearchTree **pBstree)
         return MALLOC_ERROR;
 
     }
+    /* 清除脏数据 */
     memset(bstree->root, 0, sizeof(BSTreeNode) * 1);
 
     /* 初始化根节点 */
@@ -55,10 +56,87 @@ int binarySearchTreeInit(BinarySearchTree **pBstree)
 
 }
 
+static int compareFunc(ELEMENTTYPE val1, ELEMENTTYPE val2)
+{
+   return val1 - val2;
+}
 /* 二叉搜索树的插入 */
-int binarySearchTreeInsert(BinarySearchTree **pBstree, ELEMENTTYPE val)
+int binarySearchTreeInsert(BinarySearchTree *pBstree, ELEMENTTYPE val)
 {
     int ret = 0;
+    /* 判断是否是空树 */
+    if (pBstree->size== 0)
+    {
+        (pBstree->size)++;
+
+        pBstree->root->data = val;
+        return ret;
+
+    }
+
+    /* travelNode  指向根节点 */
+    BSTreeNode * travelNode = pBstree->root;
+    BSTreeNode * parentlNode = pBstree->root;
+
+    /* 确定符号到底放在左边还是右边 */
+    int cmp = 0;
+    while (travelNode != NULL)
+    {
+        /* 标记父结点 */
+        parentlNode = travelNode;
+        cmp = compareFunc(val, travelNode->data);
+
+        /* 插入元素 < 遍历到的结点 */
+        if (val < travelNode->data)
+        {
+            travelNode = travelNode->left;
+        }
+        /* 插入元素 > 遍历到的结点 */
+        else if (val > travelNode->data)
+        {
+            travelNode = travelNode->right;
+        }
+        /* 插入元素 = 遍历到的结点 */
+        else
+        {
+            return ret;
+        }
+
+    }
+    
+    /* 分配根结点 */
+    BSTreeNode * newBstNode = (BSTreeNode *)malloc(sizeof(BSTreeNode) * 1);
+    if (newBstNode == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    memset(newBstNode, 0, sizeof(BSTreeNode) *1);
+    {
+        newBstNode->data = 0;
+        newBstNode->left = NULL;
+        newBstNode->right = NULL;
+        newBstNode->parent = NULL;
+    }
+    /* 新结点赋值 */
+    newBstNode->data = val;
+
+    /* 挂在左子树 */
+    if (cmp < 0)
+    {
+        parentlNode->left = newBstNode;
+
+    }
+    /* 挂在右子树 */
+    else 
+    {
+        parentlNode->right = newBstNode;
+    }
+
+    /* 新结点的父结点 */
+    newBstNode->parent = parentlNode;
+
+    /* 树的大小更新 */
+    (pBstree->size)++;
 
 
 
