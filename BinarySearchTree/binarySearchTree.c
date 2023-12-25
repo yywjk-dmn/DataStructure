e5ff13afc3fa01ec70fc193fa3a62a49979f08e0
yywjk-dmn <2986567641@qq.com>
2023-12-25 15:24:15 +0800

    [修改]完善树的初始化接口

--- BinarySearchTree/binarySearchTree.c
+++ BinarySearchTree/binarySearchTree.c
@@ -1,15 +1,56 @@
 #include "binarySearchTree.h"
+#include <stdlib.h>
 
-
+/* 状态码 */
+enum STATUS_CODE
+{
+    ON_SUCCESS,
+    NULL_PTR,
+    MALLOC_ERROR,
+    INVALID_ACCESS,
+    NOT_FIND,
+};
 
 
 /* 二叉搜索树的初始化 */
 int binarySearchTreeInit(BinarySearchTree **pBstree)
 {
     int ret = 0;
+    BinarySearchTree * bstree = (BinarySearchTree *)malloc(sizeof(BinarySearchTree) * 1);
+    if (bstree == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    /* 清除脏数据 */
+    memset(bstree, 0, sizeof(BinarySearchTree) * 1);
 
+    /* 初始化树 */
+    {
+        bstree->root = NULL;
+        bstree->size = 0;
+    }
+    
 
+    /* 分配根节点 */
+    bstree->root = (BSTreeNode *)malloc(sizeof(BSTreeNode) * 1);
+    if (bstree->root == NULL)
+    {
+        return MALLOC_ERROR;
 
+    }
+    memset(bstree->root, 0, sizeof(BSTreeNode) * 1);
+
+    /* 初始化根节点 */
+    {
+        bstree->root->data = 0;
+        bstree->root->left = NULL;
+        bstree->root->right = NULL;
+        bstree->root->parent = NULL;
+
+    }
+   
+
+    *pBstree = bstree;
     return ret;
 
 }
